028b8ae78b2961997e101c4f0c31e004f9aca98a
aamira97 <aamirjanyana@gmail.com>
2021-10-26 10:02:11 +0400

    class 31 code

--- users/views.py
+++ users/views.py
@@ -1,18 +1,32 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from users.forms import UserForm, NameForm
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'account created for {username}')
             return redirect('myfirstapp-home')
     else:
-        form = UserCreationForm()
+        form = UserForm()
     return render(request, 'users/register.html', {'form': form})
 
+
+def get_name(request):
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            form.save()
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+    else:
+        form = NameForm()
+    return render(request, 'name.html', {'form': form})
 
